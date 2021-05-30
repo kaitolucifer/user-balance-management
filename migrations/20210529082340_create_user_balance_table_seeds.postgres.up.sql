8d8e7d7d1aa6b4fa32ae419fbe8cf1017667a9b9
kaitolucifer <kaitolucifer@ruri.waseda.jp>
2021-05-30 14:16:50 +0900

    [add]sodaで生成したDBマイグレーションを追加

---
+++ migrations/20210529082340_create_user_balance_table_seeds.postgres.up.sql
@@ -0,0 +1,6 @@
+INSERT INTO user_balance (user_id, balance, created_at, updated_at) VALUES
+('test_user1', 100000, '2021-05-29', '2021-05-29'),
+('test_user2', 200000, '2021-05-29', '2021-05-29'),
+('test_user3', 250000, '2021-05-29', '2021-05-29'),
+('test_user4', 150000, '2021-05-29', '2021-05-29'),
+('test_user5', 50000, '2021-05-29', '2021-05-29');
