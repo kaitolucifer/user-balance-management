a6a382f3166b911a0b042bb59042b51dace62a78
kaitolucifer <kaitolucifer@ruri.waseda.jp>
2021-06-05 18:10:25 +0900

    [change]grpcハンドラとhttpハンドラを切り替えられるよう変更

--- app/config_app.go
+++ app/config_app.go
@@ -9,8 +9,11 @@ import (
 
 	"github.com/kaitolucifer/user-balance-management/infrastructure"
 	"github.com/kaitolucifer/user-balance-management/injector"
-	"github.com/kaitolucifer/user-balance-management/presentation"
+	GrpcHandler "github.com/kaitolucifer/user-balance-management/presentation/grpc"
+	RestfulHandler "github.com/kaitolucifer/user-balance-management/presentation/restful"
 )
+
+var useGrpc = flag.Bool("use_grpc", true, "true to use Grpc API and false to use normal RESTful API")
 
 // DB設定
 var dbHost = flag.String("dbhost", "localhost", "database host")
@@ -21,21 +24,29 @@ var dbPort = flag.String("dbport", "5432", "database port number")
 var dbSSL = flag.String("dbssl", "disable", "use database ssl tunnel or not")
 
 var db infrastructure.DB
-var handler *presentation.UserBalanceHandler
+var restfulHandler *RestfulHandler.RestfulUserBalanceHandler
+var grpcHandler *GrpcHandler.GrpcUserBalanceHander
 var mux http.Handler
 
 func configApp() {
 	flag.Parse()
+
 	dsn := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s",
 		*dbHost, *dbPort, *dbName, *dbUser, *dbPassword, *dbSSL)
 	db = injector.InjectDatabase(dsn)
 	repo := injector.InjectRepository(db)
 	usecase := injector.InjectUsecase(repo)
 
-	app := new(presentation.App)
-	app.InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	app.ErrorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-
-	handler = injector.InjectHandler(usecase, app)
-	mux = presentation.Routes(handler)
+	if *useGrpc {
+		app := new(GrpcHandler.App)
+		app.InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+		app.ErrorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+		grpcHandler = injector.InjectGrpcHandler(usecase, app)
+	} else {
+		app := new(RestfulHandler.App)
+		app.InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+		app.ErrorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+		restfulHandler = injector.InjectRestfulHandler(usecase, app)
+		mux = RestfulHandler.Routes(restfulHandler)
+	}
 }
--- app/main.go
+++ app/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"net"
 	"net/http"
+
+	"github.com/kaitolucifer/user-balance-management/presentation/grpc/proto"
+	"google.golang.org/grpc"
 )
 
-const portNumber = ":8080"
+const restfulPortNumber = ":8080"
+const grpcPortNumber = ":50051"
 
 func main() {
 	configApp()
 	defer db.Close()
-	handler.App.InfoLog.Printf("starting application on port %s\n", portNumber)
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: mux,
-	}
+	if *useGrpc {
+		listener, err := net.Listen("tcp", "0.0.0.0"+grpcPortNumber)
+		if err != nil {
+			grpcHandler.App.ErrorLog.Fatalf("failed to listen: %s", err)
+		}
+		grpcHandler.App.InfoLog.Printf("starting gRPC application on port %s\n", grpcPortNumber)
+		s := grpc.NewServer()
+		proto.RegisterUserBalanceServer(s, grpcHandler)
+		grpcHandler.App.ErrorLog.Fatal(s.Serve(listener))
+	} else {
+		restfulHandler.App.InfoLog.Printf("starting RESTful application on port %s\n", restfulPortNumber)
+		srv := &http.Server{
+			Addr:    restfulPortNumber,
+			Handler: mux,
+		}
 
-	handler.App.ErrorLog.Fatal(srv.ListenAndServe())
+		restfulHandler.App.ErrorLog.Fatal(srv.ListenAndServe())
+	}
 }
